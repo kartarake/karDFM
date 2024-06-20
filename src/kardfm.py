ca0337c26c3646b351aeae2a7ea06125512f7cbe
kartarake <kar.prahveen@gmail.com>
2024-06-20 23:06:54 +0530

    Commenting it out!

--- src/kardfm.py
+++ src/kardfm.py
@@ -13,7 +13,17 @@ kardfm_supports = [
 ]
 
 class kardfm:
+    """## This is the object of karDFM which contains various methods to aid in file handling.
+    """
+
     def __init__(self, dfmname, path="local") -> None:
+        """## The constructor method for the class to set up folder & metadata.
+
+        ### Args:
+            - `dfmname (str)`: The name of the DFM.
+            - `path (str, optional)`: The path where the DFM folder should settle in. Defaults to "local".
+        """ 
+
         if path == "local":
             path = dfmname+'\\'
         os.makedirs(path, exist_ok=True)
@@ -30,16 +40,25 @@ class kardfm:
         self.dfname = None
         self.dftype = None
 
+
+
     def createdoc(self, docname, doctype="json") -> None:
+        """## To create a document / file with passed unique name. If datatype is passed then takes up that datatype or it will be json data file.
+
+        ### Args:
+            - `docname (str)`: The name of the document.
+            - `doctype (str, optional)`: The type of datafile. Defaults to "json".
+
+        ### Raises:
+            - `karDFM_TypeError`: Raises when one of the arg's data type is not correct.
+            - `karDFM_TypeDefError`: Raises when file creation of unsupported data type is requested.
+        """
+
         if not (type(docname) == str):
             raise karDFM_TypeError(f"TypeError : {docname} passed in for docname.\nOnly string data type is accepted for docname arg.")
-        else:
-            pass
         
         if not (doctype in kardfm_supports):
             raise karDFM_TypeDefError(f"TypeDefError : {doctype} is not supported. \nOnly pass in document types which are supported by karDFM")
-        else:
-            pass
 
         if doctype == "json":
             docpath = self.path + docname + ".json"
@@ -66,19 +85,32 @@ class kardfm:
 
         self.loaddoc(docname)
 
+
+        
     def loaddoc(self, docname, key=None, return_value=False) -> None:
+        """## This is to load the data from a document of DFM. 
+
+        ### Args:
+            - `docname (str)`: The name of the document you want to load.
+            - `key (bytes, optional)`: The key that was used to encrypt the data. Defaults to None.
+            - `return_value (bool, optional)`: If you want the data to be returned insted of loading it up in class's variable. Defaults to False.
+
+        ### Raises:
+            - `karDFM_TypeError`: Raises when one of the arg's data type is not correct.
+            - `karDFM_DocNotFoundError`: Raises when the document name specified was not found.
+            - `karDFM_KeyNotPassed`: Raises when the key is not passed when trying to load an encrypted file.
+
+        ### Returns:
+            - `data`: The data stored in that file if return_value arg is set to True.
+        """        
         if not (type(docname) == str):
             raise karDFM_TypeError(f"TypeError : {docname} passed in for docname.\nOnly string data type is accepted for docname arg.")
-        else:
-            pass
 
         with open(self.metadatapath,'r') as f:
             metadata = json.load(f)
 
         if not(docname in metadata):
             raise karDFM_DocNotFoundError(f"DocNotFoundError : No document found in the name '{docname}'")
-        else:
-            pass
 
         doctype = metadata[docname]["doctype"]
 
@@ -110,26 +142,67 @@ class kardfm:
             self.dftype = doctype
         else:
             return data
+        
+
 
     def fetchdoclist(self):
+        """## To fetch all the names of the document created in DFM in the form of tuple.
+
+        ### Returns:
+            - `tuple`: The tuple containing names of document.
+        """        
         with open(self.metadatapath, "r") as f:
             return tuple(json.load(f).keys())
         
+
+        
     def ifdocexist(self, docname):
-        if docname in self.fetchdoclist() and docname in self.fetchmetadata():
+        """## To check if there is a document with passed in name in DFM.
+
+        ### Args:
+            - `docname (str)`: The name of the document.
+
+        ### Returns:
+            - `bool`: True if that document exists, False if not.
+        """        
+        if docname in self.fetchdoclist():
             return True
         else:
             return False
         
+
+        
     def fetchmetadata(self):
+        """## To fetch the metadata of all the files stored in DFM in the form of JSON / python dictionary.
+
+        ### Returns:
+            - `dict`: The metadata of all files.
+        """        
         with open(self.metadatapath, "r") as f:
             return json.load(f)
         
+
+        
     def putmetadata(self,metadata):
+        """## To save the passed in data to metadata file.
+
+        ### Args:
+            - `metadata (dict)`: The metadata of all files.
+        """        
         with open(self.metadatapath, "w") as f:
             json.dump(metadata,f,indent=3)
+
+
         
     def savedoc(self, key=None):
+        """## To save the data to disk from the class's variable.
+
+        ### Args:
+            - `key (bytes, optional)`: The key used for encryption of the file if it is encrypted file. Defaults to None.
+
+        ### Raises:
+            - `karDFM_KeyNotPassed`: If it was an encrypted file but key was not passed.
+        """        
         metadata = self.fetchmetadata()
         if metadata[self.dfname]["encrypted"] and not key:
             raise karDFM_KeyNotPassed(f"There was no key passed when requesting to load an encrypted file {self.dfname}")
@@ -150,8 +223,20 @@ class kardfm:
         elif self.dftype == "bin":
             with open(self.path + self.dfname + ".bin", "wb") as f:
                 pickle.dump(self.data, f)
+
+
     
     def renamedoc(self, oldname, newname):
+        """## To rename the document to someother name in which another document exists.
+
+        ### Args:
+            - `oldname (str)`: The current name of the document.
+            - `newname (str)`: The replacement name.
+
+        ### Raises:
+            - `karDFM_TypeError`: Raises if any of the arg's data type is not correct.
+            - `karDFM_DocNotFoundError`: Raises if the mentioned document was not found.
+        """        
         if not (type(oldname) == str and type(newname) == str):
             raise karDFM_TypeError("Only string data type is accepted for oldname & newname arg")
         
@@ -163,7 +248,14 @@ class kardfm:
         
         os.rename(self.path + oldname + "." + self.dftype, self.path + newname + "." + self.dftype)
 
+
+
     def deletedoc(self, docname):
+        """## To delete the document with the name passed in as arg from DFM.
+
+        ### Args:
+            - `docname (str)`: The name of the document.
+        """        
         if not(docname in self.fetchdoclist()):
             karDFM_DocNotFoundError(f"Document {docname} was not found.")
 
@@ -180,10 +272,28 @@ class kardfm:
 
         os.remove(self.path + docname + "." + doctype)
 
+
+
     def generate_key(self):
+        """## To generate a safe key and to return it. This can be used for encryption.
+
+        ### Returns:
+            - `bytes`: A 32 byte long key.
+        """        
         return security.fetchkey()
+
+
     
     def lockdoc(self, docname, key):
+        """## To lock the document by encrypting the data inside it.
+
+        ### Args:
+            - `docname (str)`: The name of document you want to encrypt.
+            - `key (bytes)`: The key you want to use for encryption.
+
+        ### Raises:
+            - `karDFM_DocNotFoundError`: Raises if no document were to be found with passed in name.
+        """   
         if not docname in self.fetchdoclist():
             raise karDFM_DocNotFoundError(f"Document with name {docname} was not found.")
         
@@ -203,7 +313,20 @@ class kardfm:
         metadata[docname]["encrypted"] = True
         self.putmetadata(metadata)
 
+
+
     def unlockdoc(self, docname, key):
+        """## To unlock the document which has been encrypted back to normal.
+
+        ### Args:
+            - `docname (str)`: The name of the encrypted document.
+            - `key (bytes)`: The key which has been used for encryption.
+
+        ### Raises:
+            - `karDFM_DocNotFoundError`: Raises if that document mentioned was not found.
+            - `karDFM_WrongKeyError`: Raises if wrong key was passed or the encrypted data was damaged.
+            - `karDFM_DocNotEncrypted`: Raises if the document passed was not encrypted to be unlocked.
+        """        
         if not docname in self.fetchdoclist():
             raise karDFM_DocNotFoundError(f"Document with name {docname} was not found.")
         
