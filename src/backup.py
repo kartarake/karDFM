811a08d24e3b88e7e2f61674eb243ca60db136e5
kartarake <kar.prahveen@gmail.com>
2024-06-24 17:57:57 +0530

    Little debugging!

--- src/backup.py
+++ src/backup.py
@@ -4,7 +4,7 @@ def createfull(path,bpath,metadata):
     inv_path = path[::-1]
     dfname = inv_path.split(".",1)[1].split("\\",1)[0][::-1]
 
-    timechar = time.strftime("%Y.%m.%d %H:%M:%S")
+    timechar = time.strftime("%Y%m%d%H%M%S")
     bfname = dfname + timechar
 
     if not bpath[-1] in ("\\","/"):
@@ -16,7 +16,8 @@ def createfull(path,bpath,metadata):
     with open(path,"rb") as f:
         data = f.read()
 
-    with open(bfpath + bfname + ".bak", "wb") as f:
+    path = bfpath + bfname + ".bak"
+    with open(path, "wb") as f:
         f.write(data)
 
     metadata["backups"].append(bfname)
@@ -25,10 +26,11 @@ def createfull(path,bpath,metadata):
 
 def loadfull(path,bpath,metadata,loadindex=-1):
     bfname = metadata["backups"][loadindex]
+    dfname = bfname[:-14]
     if not bpath[-1] in ("\\","/"):
         bpath = bpath + "\\"
 
-    with open(bpath + bfname + ".bak", "rb") as f:
+    with open(bpath + dfname + "\\" + bfname + ".bak", "rb") as f:
         data = f.read()
 
     with open(path, "wb") as f:
