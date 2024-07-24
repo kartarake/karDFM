686b4024753d45418e8af47e03f90420cbbe7492
kartarake <kar.prahveen@gmail.com>
2024-07-24 18:55:41 +0530

    Peek, size, empty? full?

--- src/karstack/strstack.pyx
+++ src/karstack/strstack.pyx
@@ -15,16 +15,16 @@ cdef class UnderflowError(Exception):
         return self.message
 
 cdef class strstack:
-    cdef public int size
-    cdef public int top
-    cdef public char** data
+    cdef int maxsize
+    cdef int top
+    cdef char** data
 
-    def __cinit__(self, int size):
-        self.size = size
+    def __cinit__(self, int maxsize):
+        self.maxsize = maxsize
         self.top = -1
-        self.data = <char**> malloc(size * sizeof(char*))
+        self.data = <char**> malloc(maxsize * sizeof(char*))
         if self.data == NULL:
-            raise MemoryError(f"Unable to allocate a stack of size {self.size} in memory")
+            raise MemoryError(f"Unable to allocate a stack of size {self.maxsize} in memory")
         
     def __dealloc__(self):
         for i in range(self.top + 1):
@@ -33,8 +33,8 @@ cdef class strstack:
 
     cpdef void push(self, str string):
         if self.top == self.size - 1:
-            raise OverflowError(f"The stack has reached the maximum size of {self.size}")
-        length = len(string)
+            raise OverflowError(f"The stack has reached the maximum size of {self.maxsize}")
+        length = strlen(string)
         self.data[self.top] = <char*> malloc((length + 1) * sizeof(char))
         if self.data[self.top] == NULL:
             raise MemoryError(f"Unable to allocate memory for the string {string}")
@@ -42,9 +42,30 @@ cdef class strstack:
 
     cpdef str pop(self):
         if self.top == -1:
-            raise UnderflowError("The stack no value to be poped out")
+            raise UnderflowError("The stack has no value to be poped out")
         bstring = self.data[self.top]
         string = bstring.decode("utf-8")
         free(bstring)
         self.top -= 1
         return string
+
+    cpdef str peek(self):
+        if self.top == -1:
+            raise UnderflowError("The stack has no value to be poped out")
+        string = self.data[self.top].decode("utf-8")
+        return string
+
+    cpdef int size(self):
+        return self.top + 1
+
+    cpdef int percentage(self):
+        return self.size() / self.maxsize * 100
+
+    cpdef bint is_empty(self):
+        return self.top == -1
+    
+    cpdef bint is_full(self):
+        return self.top == self.maxsize - 1
+
+    def __repr__(self) -> str:
+        return f"String stack: {self.top + 1}/{self.maxsize}"
